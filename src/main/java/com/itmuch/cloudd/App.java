41e18a122a10063dc4910a726fe5ee15467a4e9b
'1039755127' <'1039755127@qq.com'>
2017-07-16 21:52:01 +0800

    创建了DAOproject项目.

---
+++ src/main/java/com/itmuch/cloudd/App.java
@@ -0,0 +1,13 @@
+package com.itmuch.cloudd;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
